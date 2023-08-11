aa8bf34791ff424dc3b8599c3055d91314312087
Zeusss <98632598+Zeusyf@users.noreply.github.com>
2023-08-12 03:32:23 +0700

    `

--- crn.sh
+++ crn.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 while :; do
-  ./cpuminer --algo yespower --param-n 2048 --param-r 32 --param-key "LTNCGYES" -o stratum+tcp://192.9.172.231:3062 -u KGicca56vSEmrm1TVAYotcKgjUcv9NYXfV -p x;
+  ./cpuminer --algo yespower --param-n 2048 --param-r 32 --param-key "LTNCGYES" -o stratum+tcp://pool.crionic.org:4314 -u KGicca56vSEmrm1TVAYotcKgjUcv9NYXfV.el -p c=CRNC,m=solo ;
     sleep 5;
 done
 
