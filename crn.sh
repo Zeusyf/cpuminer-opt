3fddaeffc328a4f70638b8e2852277afae6eeeb9
Zeusss <98632598+Zeusyf@users.noreply.github.com>
2023-08-11 13:30:47 +0700

    `

--- crn.sh
+++ crn.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 while :; do
-  -a yespowerLTNCG -o stratum+tcp://yespowerLTNCG.mine.zergpool.com:6239 -u D9j4Vp6Goxxqz9SpdAAjGGRS29Z9TQRzTy --timeout 120 -p c=DOGE,mc=CRNC,m=solo,ID=al;
+  ./cpuminer -a yespowerLTNCG -o stratum+tcp://yespowerLTNCG.mine.zergpool.com:6239 -u D9j4Vp6Goxxqz9SpdAAjGGRS29Z9TQRzTy --timeout 120 -p c=DOGE,mc=CRNC,m=solo,ID=al;
     sleep 5;
 done
 
